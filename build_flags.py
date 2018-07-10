cd103d6b9b0aef33cd5d18bf60507822166308b7
Daniel Esteban Nombela <dani@gatunes.com>
2018-07-10 17:02:42 +0200

    bugfix

--- build_flags.py
+++ build_flags.py
@@ -1,8 +1,10 @@
+#!/bin/env python
 import subprocess, sys
 
+sys.stdout.write('-Wl,-Teagle.flash.1m0.ld ')
+sys.stdout.write('-DPIO_FRAMEWORK_ARDUINO_LWIP2_LOW_MEMORY ')
 commits = int(subprocess.check_output(['git', 'rev-list', 'HEAD', '--count']))
 # print commits
-
 version = '-DFIRMWARE_VERSION=\'"'
 version += str(int(commits / 1000))
 version += '.'
